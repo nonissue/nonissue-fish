ad065f88b7dab05c2faabec6d7eb8438893a4096
Andrew Williams <williamsa79@mymacewan.ca>
2018-06-24 21:53:43 -0600

    removed a lot of unneeded code

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -38,28 +38,7 @@ function __nonissue_colors -S -a color_scheme -d 'Define colors used by nonissue
       set -x non_segment_exit               $grey[3] $grey[1]
 
       # the rest
-      set -x color_initial_segment_exit     $grey[3] $grey[1]
-      set -x color_initial_segment_su       $white $green[2] --bold
-      set -x color_initial_segment_jobs     $white $blue[3] --bold
-      
-      set -x color_path                     $grey[3] $grey[2]
-      set -x color_path_basename            $grey[3] $white --bold
-      set -x color_path_nowrite             $red[3] $red[1]
-      set -x color_path_nowrite_basename    $red[3] $red[1] --bold
-
-      set -x color_repo                     $green[1] $green[3]
-      set -x color_repo_work_tree           $grey[3] $white --bold
-      set -x color_repo_dirty               $red[2] $white
-      set -x color_repo_staged              $orange[1] $orange[3]
-
-      set -x color_vi_mode_default          $grey[2] $grey[3] --bold
-      set -x color_vi_mode_insert           $green[2] $grey[3] --bold
-      set -x color_vi_mode_visual           $orange[1] $orange[3] --bold
-
-      set -x color_k8s                      $green[2] $white --bold
-      set -x color_username                 $grey[1] $blue[3] --bold
-      set -x color_hostname                 $grey[1] $blue[3]
-      set -x color_virtualfish              $blue[2] $grey[1] --bold
+      # set -x color_initial_segment_exit     $grey[3] $grey[1]
   end
 end
 
@@ -73,72 +52,11 @@ function __nonissue_glyphs -S -d 'Define glyphs used by nonissue'
   set -x branch_glyph            \uF418 # different branch glyph
   set -x right_black_arrow_glyph \uE0B0
   set -x right_arrow_glyph       \uE0B1
-  set -x left_black_arrow_glyph  \uE0B2
-  set -x left_arrow_glyph        \uE0B3
 
   set -x dirty_gylph             \uF055
   set -x clean_glyph             \uF058
 
-  # Additional glyphs
-  set -x detached_glyph          \u27A6
-  set -x tag_glyph               \u2302
-  set -x nonzero_exit_glyph      '! '
-  set -x superuser_glyph         '$ '
-  set -x bg_job_glyph            '% '
-  set -x hg_glyph                \u263F
-
-  # Python glyphs
-  set -x superscript_glyph       \u00B9 \u00B2 \u00B3
-  set -x virtualenv_glyph        \u25F0
-  set -x pypy_glyph              \u1D56
-  # Desk glyphs
-
-  # Git glyphs
-  set -x git_dirty_glyph      '*'
-  set -x git_staged_glyph     '~'
-  set -x git_stashed_glyph    '$'
-  set -x git_untracked_glyph  '…'
-  set -x git_ahead_glyph      \u2191 # '↑'
-  set -x git_behind_glyph     \u2193 # '↓'
-  set -x git_plus_glyph       '+'
-  set -x git_minus_glyph      '-'
-  set -x git_plus_minus_glyph '±'
-
-  # Disable Powerline fonts
-  if [ "$theme_powerline_fonts" = "no" ]
-    set branch_glyph            \u2387
-    set right_black_arrow_glyph ''
-    set right_arrow_glyph       ''
-    set left_black_arrow_glyph  ''
-    set left_arrow_glyph        ''
-  end
-
-  # Use prettier Nerd Fonts glyphs
-  if [ "$theme_nerd_fonts" = "yes" ]
-    set branch_glyph     \uF418
-    set detached_glyph   \uF417
-    set tag_glyph        \uF412
-
-    set git_dirty_glyph      \uF448 '' # nf-oct-pencil
-    set git_staged_glyph     \uF0C7 '' # nf-fa-save
-    set git_stashed_glyph    \uF0C6 '' # nf-fa-paperclip
-    set git_untracked_glyph  \uF128 '' # nf-fa-question
-    # set git_untracked_glyph  \uF141 '' # nf-fa-ellipsis_h
-
-    set git_ahead_glyph      \uF47B # nf-oct-chevron_up
-    set git_behind_glyph     \uF47C # nf-oct-chevron_down
-
-    set git_plus_glyph       \uF0DE # fa-sort-asc
-    set git_minus_glyph      \uF0DD # fa-sort-desc
-    set git_plus_minus_glyph \uF0DC # fa-sort
-  end
-
-  # Avoid ambiguous glyphs
-  if [ "$theme_avoid_ambiguous_glyphs" = "yes" ]
-    set git_untracked_glyph '...'
-  end
 end
-
 
 # copied from bobthefish
 # function to print our separator and set BG if we wish
@@ -257,11 +175,4 @@ function fish_prompt -d 'nonissue, a mod of several existing themes'
   set color normal
   echo -n ' '
   
-  
-  # set -l colors 333 666 aaa 
-  # for color in $colors
-  #   echo -n (set_color $color)">"
-  # end
-  
-
 end
